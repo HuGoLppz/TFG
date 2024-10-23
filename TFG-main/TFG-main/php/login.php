c41ba69de787a6eea6b745a57ccf2b9843e57fde
Hugo López Sanz <154275546+HuGoLppz@users.noreply.github.com>
2024-10-23 14:35:22 +0200

    Actualización-curro

---
+++ TFG-main/TFG-main/php/login.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+
+$host = 'localhost';  
+$dbname = 'study_planner';
+$username = 'root';   
+$password = '';       
+
+try {
+    // Crear conexión a la base de datos
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $name = $_POST['email'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        
+        // Consultar el usuario en la base de datos
+        $stmt = $conn->prepare("SELECT usuario_id, password FROM Usuarios WHERE email = :email LIMIT 1");
+        $stmt->bindParam(':email', $email);
+        $stmt->execute();
+
+        // Verificar si el usuario existe
+        if ($stmt->rowCount() == 1) {
+            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+            
+            // Verificar la contraseña
+            if (password_verify($password, $user['password'])) {
+                $_SESSION['usuario_id'] = $user['usuario_id'];
+                echo json_encode(['success' => true, 'message' => 'Inicio de sesión exitoso']);
+            } else {
+                echo json_encode(['success' => false, 'message' => 'Contraseña incorrecta']);
+            }
+        } else {
+            echo json_encode(['success' => false, 'message' => 'Correo no registrado']);
+        }
+    }
+} catch (PDOException $e) {
+    echo json_encode(['success' => false, 'message' => 'Error al conectar con la base de datos']);
+}
+?>
