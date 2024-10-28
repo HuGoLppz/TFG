b7e02dd2bae98d7ca74db975fc1e581928f19749
Hugo López Sanz <154275546+HuGoLppz@users.noreply.github.com>
2024-10-28 10:17:11 +0100

    prueba revertida

--- php/login.php
+++ php/login.php
@@ -8,7 +8,6 @@ $password = '';
 
 try {
     // Crear conexión a la base de datos
-    //holi
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
